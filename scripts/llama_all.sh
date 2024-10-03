96f7579af05d022dd5ba141d03a6528a65c109b6
Sean I Young <siy0@kale.nmr.mgh.harvard.edu>
2024-10-03 13:06:39 -0400

    update llama script

--- scripts/llama_all.sh
+++ scripts/llama_all.sh
@@ -6,18 +6,18 @@ valid_size=64
 stride=128
 # bitrate=3.00
 pca=1024
-gpus=4
+gpus=1
 max_iters=256 #1024
 checkpointing="--checkpointing"
 
-for model in Llama-2-70b-hf Llama-2-13b-hf Llama-2-7b-hf
+for model in Llama-2-70b-hf # Llama-2-70b-hf
 do
-    for bitrate in 4.0 3.0 #3.0 4.0 # 4.0 # 2.1 2.2 2.4 2.6 2.8 # 3.0 3.2 3.4 3.6 3.8 4.0 #4.0 3.0
+    for bitrate in 3.0 4.0
     do
-	for batch_size in 16 # 16 8 4 2 # 8 4 2 1
+	for batch_size in 16
 	do
-	    remarks="$class"-"$model"_group_size_"$group_size"_batch_size_"$batch_size"_bit_rate_"$bitrate"_stride_"$stride"_max_iters_"$max_iters"_debug_final #_checkpointing
-	    python opt_train.py $checkpointing --model_id $class/$model --group_size $group_size --batch_size $batch_size --valid_size $valid_size --bitrate $bitrate --train_tokens 128 --max_iters $max_iters --stride $stride --pca $pca --gpus $gpus --remarks $remarks | tee -a results/opt_all/$remarks.txt
+	    remarks="$class"-"$model"_group_size_"$group_size"_batch_size_"$batch_size"_bit_rate_"$bitrate"_stride_"$stride"_max_iters_"$max_iters"_timing #_checkpointing
+	    python opt_train.py $checkpointing --model_id $class/$model --group_size $group_size --batch_size $batch_size --valid_size $valid_size --bitrate $bitrate --train_tokens 128 --max_iters $max_iters --stride $stride --pca $pca --gpus $gpus --remarks $remarks | tee -a results/llama_all/$remarks.txt
 	done
     done
 done
